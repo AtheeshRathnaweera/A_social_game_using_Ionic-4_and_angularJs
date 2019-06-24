380ecea07ad2ad7f2054dd2b2bf19d9c148c8a65
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-06-24 23:04:48 +0530

    Created the register page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signIn(){
+    this.router.navigate(['register'])
+
   }
 
 }
